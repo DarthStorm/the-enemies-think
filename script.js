a08042b3ae106878770b7c1282decf375d8f8b0c
DarthStorm <darthstorm08@gmail.com>
2023-09-20 19:46:17 +0800

    added temp favicon, level, black bg fixed canvas position. CSS AAAARGGHGHGGHGHGHGHGH

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-canvas =  HTMLCanvasElement();
+const canvas = document.createElement('canvas');
 canvas.parent = document.body;
-width = 1080;
-height = 720;
+const width = 1080;
+const height = 720;
 canvas.width = width;
 canvas.height = height;
 canvas.id = 'canvas';
-
-
-ctx = canvas.getContext('2d');
 
 /*
 The Enemies Think
 
 TODO: think of a better name
 
-the enemy thinks. see how long you can surive for.
+the enemy thinks. 
+
+see how long you can surive for.
 if there are loads of enemies around u and ur taking dmg a lot, enemies will stop coming
 and enemies will have a slight stat nerf
 
@@ -29,12 +28,63 @@ like +10 atk, +30% fire rate, + 5 hp, + health regen every 4 secs
 they think after 2 mins
 */
 
+
+class Player{
+    //this is a player. 
+    constructor(x,y,width=32,height=32,color = "#FF7777"){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    tick(level=lvl){
+        console.log("playertick");//i WILL use tick, trust
+    }
+    draw(level=lvl){
+        //draw player
+        level.ctx.fillStyle = this.color;
+        level.ctx.fillRect(this.x,this.y,this.width,this.height);
+    }
+}
+
+class Level{
+    constructor(name = "Untitled",canvas_ = canvas){
+        this.name = name;//idk what to do with this
+        this.canvas = canvas_;
+        this.ctx = canvas.getContext('2d');
+        
+        this.players = []
+        
+        this.players.push(new Player(0,0,32,32))
+    }
+
+    tick(){
+        //tick players
+        for (let i = 0; i < this.players.length; i++) {
+            this.players[i].tick()
+        }
+    }
+    draw(){
+        //draw bg
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        //draw players
+        for (let i = 0; i < this.players.length; i++) {
+            this.players[i].tick()   
+        }
+    }
+}
+
+
+const lvl = new Level("Level 1", canvas);
+
 function tick() {
-    
+    lvl.tick();
 }
 
 function draw(){
-
+    lvl.draw()
 }
 
 function loop(){
@@ -43,7 +93,7 @@ function loop(){
 }
 
 window.onload = () => {
-    canvas = document.getElementById("canvas");
+    document.body.appendChild(canvas);
     setInterval(loop(),30);
     console.log(canvas);
 }

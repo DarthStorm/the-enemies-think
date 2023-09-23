11303e7c18c63399906ec8ee0d5186696973b830
DarthStorm <darthstorm08@gmail.com>
2023-09-23 20:03:14 +0800

    added dev.log, added bg, player movement, enemy...
    omg where do i even start
    
    added dev.log, my diary-esque thing
    istg its gonna become very useful some time in life
    
    added the bg, it shows
    
    added player, it moves, it showws up on screen
    
    added enemies, it exists, and shows up
    
    added assets folder
    
    added customizable controls

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
-const canvas = document.createElement('canvas');
-canvas.parent = document.body;
-const width = 1080;
-const height = 720;
-canvas.width = width;
-canvas.height = height;
-canvas.id = 'canvas';
-
 /*
-The Enemies Think
+WARNING
+You probably shouldn't be here: this contains the source code for the game.
+(not like i mind... for now)
 
+The devlog is in the same directory, at dev.log
+Proceed if you're one of my teachers, or someone who wants to look at my code. (yay)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+oh you ignored my warning. well, welcome.
+this is my personal project for my IB thing ( i forgot the name )
+since you're here, you (hopefully) know how js works,
+and/or you're probably a teacher that is evaluating this.
+so
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+vscode on top
+
+The Enemies Think
 TODO: think of a better name
 
 the enemy thinks. 
@@ -28,18 +69,44 @@ like +10 atk, +30% fire rate, + 5 hp, + health regen every 4 secs
 they think after 2 mins
 */
 
+const canvas = document.createElement('canvas');
+canvas.parent = document.body;
+const width = 1080;
+const height = 720;
+canvas.width = width;
+canvas.height = height;
+canvas.id = 'canvas';
 
-class Player{
+const allControls = {
+
+};
+
+function loadImage(src = "") {
+    let x = new Image();
+    x.src = "assets/" + src;
+    return x;
+}
+const images = {
+    bg:loadImage("bg.png")
+}
+
+
+
+
+class Player {
     //this is a player. 
-    constructor(x,y,width=32,height=32,color = "#FF7777"){
+    constructor(x,y,width=32,height=32,color = "#FFFFFF"){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         this.color = color;
+        this.speed = 10;
     }
+
     tick(level=lvl){
-        console.log("playertick");//i WILL use tick, trust
+        this.x += (level.controls.right - level.controls.left) * this.speed;
+        this.y += (level.controls.down - level.controls.up) * this.speed;
     }
     draw(level=lvl){
         //draw player
@@ -48,30 +115,77 @@ class Player{
     }
 }
 
-class Level{
+class Enemy {
+    constructor(x,y,width=32,height=32,color = "#FF7777"){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.speed = 5;
+    }
+
+    tick(level=lvl){
+        var playersSorted = level.players.concat() // maxes a new array. but HOW
+        playersSorted.sort(function(a,b){
+            return Math.sqrt((b.x - this.x) ** 2 + (b.y - this.y) ** 2) - Math.sqrt((a.x - this.x) ** 2 + (a.y - this.y) ** 2) 
+        });
+    }
+
+    draw(level=lvl){
+        level.ctx.fillStyle = this.color;
+        level.ctx.fillRect(this.x,this.y,this.width,this.height);
+    }
+}
+class Level {
     constructor(name = "Untitled",canvas_ = canvas){
         this.name = name;//idk what to do with this
         this.canvas = canvas_;
         this.ctx = canvas.getContext('2d');
         
-        this.players = []
+        this.players = [];
         
-        this.players.push(new Player(0,0,32,32))
-    }
+        this.players.push(new Player(0,0,32,32));
 
+        this.enemies = [];
+
+        //test
+        this.enemies.push(new Enemy(100,100,32,32));
+
+        this.controls = {
+            up:false,
+            down:false,
+            left:false,
+            right:false
+        }
+    }
+    getControls(){
+        this.controls.up = (allControls["w"] || allControls["ArrowUp"])??false;
+        this.controls.down = (allControls["s"] || allControls["ArrowDown"])??false;
+        this.controls.left = (allControls["a"] || allControls["ArrowLeft"])??false;
+        this.controls.right = (allControls["d"] || allControls["ArrowRight"])??false;
+    }
     tick(){
+        this.getControls()
         //tick players
-        for (let i = 0; i < this.players.length; i++) {
-            this.players[i].tick()
+        for (let p = 0; p < this.players.length; p++) {
+            this.players[p].tick();
+        }
+        //tick enemies
+        for (let e = 0; e < this.enemies.length; e++) {
+            this.enemies[e].tick();
         }
     }
     draw(){
         //draw bg
-        this.ctx.fillStyle = "black";
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.drawImage(images.bg,0,0)
         //draw players
-        for (let i = 0; i < this.players.length; i++) {
-            this.players[i].tick()   
+        for (let e = 0; e < this.enemies.length; e++) {
+            this.enemies[e].draw()
+        }
+        //draw enemies
+        for (let p = 0; p < this.players.length; p++) {
+            this.players[p].draw()   
         }
     }
 }
@@ -92,8 +206,18 @@ function loop(){
     draw();
 }
 
+function addEventListeners(){
+    document.addEventListener("keydown", function(e){
+        allControls[e.key] = true;
+    })
+    document.addEventListener("keyup", function(e){
+        allControls[e.key] = false;
+    })
+}
+
 window.onload = () => {
+    addEventListeners()
     document.body.appendChild(canvas);
-    setInterval(loop(),30);
+    setInterval(loop,30);
     console.log(canvas);
 }

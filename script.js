d2fedf351154002e0b745dfe1d9a762a02bcbde6
DarthStorm <darthstorm08@gmail.com>
2023-10-28 17:00:55 +0800

    make code pretty lol

--- script.js
+++ script.js
@@ -72,7 +72,6 @@ also all this is subject to change (duh)
 this is a concept with potential
 i hope
 */
-
 const canvas = document.createElement('canvas');
 canvas.parent = document.body;
 const width = 1080;
@@ -86,10 +85,10 @@ const allControls = {
 };
 
 const mouse = {
-    down:false,
-    position:{
-        x:0,
-        y:0
+    down: false,
+    position: {
+        x: 0,
+        y: 0
     }
 }
 
@@ -99,19 +98,19 @@ function loadImage(src = "") {
     return x;
 }
 const images = {
-    bg:loadImage("bg.png")
+    bg: loadImage("bg.png")
 }
 
 
-function newWeapon (type_="",onAttack_ = (player) => {
-    if (typeof player == player) {
+function newWeapon(type_ = "", onAttack_ = (player) => {
+    if(typeof player == player) {
         // level.projectiles.push(new Projectile());
     }
-}){
+}) {
 
     const Weapon = {
-        type:type_,
-        onAttack:onAttack_
+        type: type_,
+        onAttack: onAttack_
     }
 
     return Weapon;
@@ -119,18 +118,18 @@ function newWeapon (type_="",onAttack_ = (player) => {
 
 const WEAPONS = {
     //weapons
-    Pistol:newWeapon("Pistol",function(level){
+    Pistol: newWeapon("Pistol", function(level) {
         //tmp
         console.log("hi uwu");
     }),
 
-    
+
 };
 
 
 class Player {
     //this is a player. 
-    constructor(x,y,width=32,height=32,color = "#FFFFFF",weapon = WEAPONS.Pistol){
+    constructor(x, y, width = 32, height = 32, color = "#FFFFFF", weapon = WEAPONS.Pistol) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -140,20 +139,20 @@ class Player {
         this.weapon = weapon;
     }
 
-    tick(level=lvl){
+    tick(level = lvl) {
         this.x += (level.controls.right - level.controls.left) * this.speed;
         this.y += (level.controls.down - level.controls.up) * this.speed;
     }
-    draw(level=lvl){
+    draw(level = lvl) {
         //draw player
         level.ctx.fillStyle = this.color;
-        level.ctx.fillRect(this.x,this.y,this.width,this.height);
+        level.ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
 
 class Enemy {
-    constructor(x,y,width=32,height=32,color = "#FF7777"){
+    constructor(x, y, width = 32, height = 32, color = "#FF7777") {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -162,103 +161,103 @@ class Enemy {
         this.speed = 5;
     }
 
-    tick(level=lvl){
+    tick(level = lvl) {
         var playersSorted = level.players.concat() // makes a new array. but HOW
-        playersSorted.sort(function(a,b){
-            return Math.sqrt((b.x - this.x) ** 2 + (b.y - this.y) ** 2) - Math.sqrt((a.x - this.x) ** 2 + (a.y - this.y) ** 2) 
+        playersSorted.sort(function(a, b) {
+            return Math.sqrt((b.x - this.x) ** 2 + (b.y - this.y) ** 2) - Math.sqrt((a.x - this.x) ** 2 + (a.y - this.y) ** 2)
         });
 
     }
 
-    draw(level=lvl){
+    draw(level = lvl) {
         level.ctx.fillStyle = this.color;
-        level.ctx.fillRect(this.x,this.y,this.width,this.height);
+        level.ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
 class Projectile {
-    constructor(x,y,type,direction,width,height,color){
+    constructor(x, y, type, direction, width, height, color) {
         //in future add a object ot keep track of ALL these features to better spawn bullets
         this.x = x;
         this.y = y;
         this.type = type;
         this.direction = direction
-        
+
         this.width = width;
         this.height = height;
         this.speed = speed;
         this.color = color;
     }
-    
-    tick(level=lvl){
-        this.x += Math.sin(this.direction)*5;
-        this.y -= Math.cos(this.direction)*5;
+
+    tick(level = lvl) {
+        this.x += Math.sin(this.direction) * 5;
+        this.y -= Math.cos(this.direction) * 5;
     }
 
-    draw(level=lvl){
+    draw(level = lvl) {
         level.ctx.fillStyle = this.color;
-        level.ctx.fillRect(this.x,this.y,this.width,this.height);
+        level.ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
 class Level {
-    constructor(name = "Untitled",canvas_ = canvas){
-        this.name = name;//idk what to do with this
+    constructor(name = "Untitled", canvas_ = canvas) {
+        this.name = name; //idk what to do with this
         this.canvas = canvas_;
         this.ctx = canvas.getContext('2d');
-        
+
         this.players = [];
-        
-        this.players.push(new Player(0,0,32,32));
+
+        this.players.push(new Player(0, 0, 32, 32));
 
         this.enemies = [];
 
         //test
-        this.enemies.push(new Enemy(100,100,32,32));
+        this.enemies.push(new Enemy(100, 100, 32, 32));
 
         this.projectiles = []
 
         this.controls = {
-            up:false,
-            down:false,
-            left:false,
-            right:false,
-            shoot:false,
+            up: false,
+            down: false,
+            left: false,
+            right: false,
+            shoot: false,
 
-            mousePosition:{
-                x:0,
-                y:0,
+            mousePosition: {
+                x: 0,
+                y: 0,
             },
 
         }
     }
-    getControls(){
-        this.controls.up = (allControls["w"] || allControls["ArrowUp"])??false;
-        this.controls.down = (allControls["s"] || allControls["ArrowDown"])??false;
-        this.controls.left = (allControls["a"] || allControls["ArrowLeft"])??false;
-        this.controls.right = (allControls["d"] || allControls["ArrowRight"])??false;
+    getControls() {
+        this.controls.up = (allControls["w"] || allControls["ArrowUp"]) ?? false;
+        this.controls.down = (allControls["s"] || allControls["ArrowDown"]) ?? false;
+        this.controls.left = (allControls["a"] || allControls["ArrowLeft"]) ?? false;
+        this.controls.right = (allControls["d"] || allControls["ArrowRight"]) ?? false;
     }
-    tick(){
+    tick() {
         this.getControls()
         //tick players
-        for (let p = 0; p < this.players.length; p++) {
+        for(let p = 0; p < this.players.length; p++) {
             this.players[p].tick();
         }
         //tick enemies
-        for (let e = 0; e < this.enemies.length; e++) {
+        for(let e = 0; e < this.enemies.length; e++) {
             this.enemies[e].tick();
         }
     }
-    draw(){
+    draw() {
         //draw bg
-        this.ctx.drawImage(images.bg,0,0)
+        this.ctx.drawImage(images.bg, 0, 0)
         //draw players
-        for (let e = 0; e < this.enemies.length; e++) {
+        for(let e = 0; e < this.enemies.length; e++) {
             this.enemies[e].draw()
         }
         //draw enemies
-        for (let p = 0; p < this.players.length; p++) {
-            this.players[p].draw()   
+        for(let p = 0; p < this.players.length; p++) {
+            this.players[p].draw()
         }
     }
 }
@@ -270,30 +269,30 @@ function tick() {
     lvl.tick();
 }
 
-function draw(){
+function draw() {
     lvl.draw()
 }
 
-function loop(){
+function loop() {
     tick();
     draw();
-    console.log(mouse,mouse.position);
+    console.log(mouse, mouse.position);
 }
 
-function addEventListeners(){
-    document.addEventListener("keydown", function(e){
+function addEventListeners() {
+    document.addEventListener("keydown", function(e) {
         allControls[e.key] = true;
     })
-    document.addEventListener("keyup", function(e){
+    document.addEventListener("keyup", function(e) {
         allControls[e.key] = false;
     })
-    document.addEventListener("mousedown", function(e){
+    document.addEventListener("mousedown", function(e) {
         mouse.down = true;
     });
-    document.addEventListener("mouseup", function(e){
+    document.addEventListener("mouseup", function(e) {
         mouse.down = false;
     });
-    document.addEventListener("mousemove", function(e){
+    document.addEventListener("mousemove", function(e) {
         mouse.position.x = e.offsetX;
         mouse.position.y = e.offsetY;
     });
@@ -304,7 +303,7 @@ function addEventListeners(){
 window.onload = () => {
     addEventListeners()
     document.body.appendChild(canvas);
-    setInterval(loop,30);
+    setInterval(loop, 30);
     console.log(canvas);
-    
+
 }
